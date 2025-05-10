e96a68ea5fc420bd9dd24218a5b09b2ca7f1ff67
Qijun Feng <folklorers5@gmail.com>
2025-05-10 15:32:36 +0200

    linear and nonlinear gray box
    bad results

---
+++ linear_reaction_pendulum.m
@@ -0,0 +1,6 @@
+function [A,B,C,D] = linear_reaction_pendulum(J_theta, mu_theta,J_phi, mu_phi,kt,M,L, g,Ts)
+A = [0, 0, 1; 0, -mu_phi/J_phi, 0; -(M*g*L)/J_theta, mu_phi/J_theta, -mu_theta/J_theta];
+B = [0; kt/J_phi; -kt/J_theta];
+C = [1, 0, 0; 0, 1, 0];
+D = zeros(2,1);
+end 
